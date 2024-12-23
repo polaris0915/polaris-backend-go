35a6904372368f1ebc3ace6caf32ae870a8d8bd2
ALin-SR <30888489@qq.com>
2024-12-23 09:09:43 +0800

    feat: modify the register request struct and add UserEmail to it

--- api/user/user.go
+++ api/user/user.go
@@ -5,14 +5,15 @@ import (
 	"net/http"
 )
 
-type UserJson struct {
-	Name     string `json:"name" binding:"required,max=255"`
-	Password string `json:"password" binding:"max=255"`
+type Register struct {
+	UserAccount  string `json:"userAccount"`
+	UserPassword string `json:"userPassword"`
+	UserEmail    string `json:"userEmail"`
 }
 
 // AddUser /api/users POST
 func AddUser(c *gin.Context) {
-	var json UserJson
+	var json Register
 	if err := c.ShouldBindJSON(&json); err != nil {
 		c.JSON(http.StatusNotAcceptable, gin.H{
 			"message": "Requested with wrong parameters",
@@ -20,4 +21,8 @@ func AddUser(c *gin.Context) {
 		})
 		return
 	}
+
+	// 处理用户注册的数据
+	// 校验用的账号，密码等等信息
+	// 校验完成之后没有问题，就要将数据插入到数据库
 }
